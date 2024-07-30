915c32721b52d702317bd91004eefbcc4c208819
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2024-07-29 19:07:24 -0700

    correct error

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,7 +6,7 @@ from sys import argv
 if __name__ == "__main__":
     api = "https://jsonplaceholder.typicode.com"
     if len(argv) > 1:
-        employee = requests.get(f'{api}/users/{int(argv[1])}').json()["name"]
+        employee_name = requests.get(f'{api}/users/{int(argv[1])}').json()["name"]
         total_tasks = requests.get(f'{api}/todos?userId={int(argv[1])}').json()
         completed_tasks = [
                 task for task in total_tasks
@@ -14,7 +14,7 @@ if __name__ == "__main__":
                 ]
         print(
                 f"Employee {employee_name} is done with tasks("
-                f"{len(completed_tasks)}/{len(todos)}):"
+                f"{len(completed_tasks)}/{len(total_tasks)}):"
                 )
         for task in completed_tasks:
             print(f"\t {task['title']}")
